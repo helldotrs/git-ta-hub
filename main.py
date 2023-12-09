125b2bc339e7d3e7b9658761d8e198a1a2c5d715
Rebekah <macrosystem@proton.me>
2023-12-09 16:28:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-output    = ""
-username  = input("username:")
-useremail = input("useremail:")
+username    = input("username:")
+useremail   = input("useremail:")
+passphrase  = input
 
-output += "git config --global user.name \"" + username + "\"\n"
-output += "git config --global user.email \"" + useremail + "\"\n"
+print("run:git config --global user.name \"" + username + "\"\n
+git config --global user.email \"" + useremail + "\"")
+
+#FIXME: autorun
+print("run: ssh-keygen -t rsa -b 4096 -C \"" + useremail + "\"")
+print("hit:enter")
+print("optional, type passphrase, enter, passphrase")
+print("hit [enter]")
+print("run:cat ~/.ssh/id_rsa.pub")
+print("copy output")
+print("go  to: https://github.com/settings/ssh/new")
+print("give name, paste, add SSH key")
+print("MFA")
+print("run to test: ssh -T git@github.com")
+
+#git clone git@github.com:your-username/your-repository.git
+
+
 
 
 #end of program:
