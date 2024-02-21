07df5ec389ac311009b1338f6477d8ace82ec539
Rebekah <macrosystem@proton.me>
2024-02-21 01:52:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ passphrase  = input("ssh passphrase (NOT YOUR GH PW):")
 if input("again:") != passphrase:
     exit("does not match, exiting")
 
-print(f'''run:git config --global user.name "{username}" && git config --global user.email "{useremail}"') && ssh-keygen -t rsa -b 4096 -C "{useremail}"
-    
-    ''')
+print(f'run:git config --global user.name "{username}" && git config --global user.email "{useremail}" && ssh-keygen -t rsa -b 4096 -C "{useremail}"')
+
 print("hit:enter")
 print("optional, type passphrase, enter, passphrase")
 print("hit [enter]")
