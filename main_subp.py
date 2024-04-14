82c97c52e4cc1c8ccdb49f5c6e647fb12ee7ace8
Rebekah <macrosystem@proton.me>
2024-04-15 01:41:58 +0200

    Update main_subp.py
    minor bugfix

--- main_subp.py
+++ main_subp.py
@@ -6,7 +6,7 @@ def get_input(prompt, confirm = False):
     user_input = input(f"{prompt}: ")
 
     if confirm:
-        if input("Confirm: " != user_input:
+        if input("Confirm: " != user_input):
             exit("confirmation failed. exiting.")
 
     return user_input
