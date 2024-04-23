c01995fa3eacbe404fdc7429628153be51e6fdd0
Rebekah <macrosystem@proton.me>
2024-04-23 20:33:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,19 @@ useremail   = input("useremail:")
 #if input("again:") != passphrase:
 #    exit("does not match, exiting")
 
-print(f'run:git config --global user.name "{username}" && git config --global user.email "{useremail}" && ssh-keygen -t rsa -b 4096 -C "{useremail}"')
+print(f'run:\ngit config --global user.name "{username}" && git config --global user.email "{useremail}" && ssh-keygen -t rsa -b 4096 -C "{useremail}"')
 
+print(" ")
 print("hit:enter")
 print("optional, type passphrase, enter, passphrase")
 print("hit [enter]")
-print("run:cat ~/.ssh/id_rsa.pub")
+print("run:\ncat ~/.ssh/id_rsa.pub")
+print(" ")
 print("copy output")
 print("go  to: https://github.com/settings/ssh/new")
 print("give title, paste, add SSH key") #FIXME: ask for title?
 print("MFA")
-print("run to test: ssh -T git@github.com")
+print("run to test:\nssh -T git@github.com")
 
 #git clone git@github.com:your-username/your-repository.git
 
