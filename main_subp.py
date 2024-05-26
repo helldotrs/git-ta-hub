243881960c313432f8955a5a02c847ba14faceb9
Rebekah <macrosystem@proton.me>
2024-05-26 21:50:58 +0200

    Update main_subp.py

--- main_subp.py
+++ main_subp.py
@@ -1,15 +1,22 @@
 import subprocess
 
+def run_subprocess(execute_this):
+    try: 
+        # shell=True for single single string
+        subprocess.run(execute_this, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f'error: {e}')
+
 print ("Warning: any input will be kept unencryted in ram while this software is running.")
 print (" ")
 
-username    = input("username:")
-useremail   = input("useremail:")
+username     = input("username:")
+useremail    = input("useremail:")
 
-print(f'run:\ngit config --global user.name "{username}" && git config --global user.email "{useremail}" && ssh-keygen -t rsa -b 4096 -C "{useremail}"')
+run_subprocess(f'git config --global user.name "{username}" && git config --global user.email "{useremail}" && ssh-keygen -t rsa -b 4096 -C "{useremail}"')
 
-
-print("run:\ncat ~/.ssh/id_rsa.pub")
+copy
+cat ~/.ssh/id_rsa.pub")
 print(" ")
 print("copy output")
 print("go  to: https://github.com/settings/ssh/new")
