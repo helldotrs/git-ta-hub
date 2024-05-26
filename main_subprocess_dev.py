53e4e1a2f5cef5ddfe3a784891e32907828363d1
Rebekah <macrosystem@proton.me>
2024-05-26 22:42:10 +0200

    Update main_subprocess_dev.py

--- main_subprocess_dev.py
+++ main_subprocess_dev.py
@@ -1,17 +1,7 @@
 import subprocess
 
 def run_subprocess(execute_this, request_output = False):
-    if not request_output:
-        try: 
-            # shell=True for single single string
-            subprocess.run(execute_this, shell=True, check=True)
-            
-        except subprocess.CalledProcessError as e:
-            print(f'error: {e}')
-            print("error id: smol penguin")
-            print(f"str: {execute_this}")
-            
-    else:
+    if request_output:
         try:
 
             result = subprocess.run(execute_this, shell=True, check=True, capture_output=True, text=True)
@@ -22,9 +12,15 @@ def run_subprocess(execute_this, request_output = False):
             print(f"stderr: {e.stderr}")
             print("error it: big ant")
             print(f"str: {execute_this}")
-                
-        
-
+    else:
+        try: 
+            # shell=True for single single string
+            subprocess.run(execute_this, shell=True, check=True)
+            
+        except subprocess.CalledProcessError as e:
+            print(f'error: {e}')
+            print("error id: smol penguin")
+            print(f"str: {execute_this}")
 
 print ("Warning: any input will be kept unencryted in ram while this software is running.")
 print (" ")
